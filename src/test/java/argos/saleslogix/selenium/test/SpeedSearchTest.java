c34505670708d7564511d1c84ac88bd03158729f
mllena321 <mike.llena@sage.com>
2013-09-17 07:24:22 -0700

    removing un-necessary .class files

--- src/test/java/argos/saleslogix/selenium/test/SpeedSearchTest.java
+++ src/test/java/argos/saleslogix/selenium/test/SpeedSearchTest.java
@@ -1,23 +1,9 @@
 package argos.saleslogix.selenium.test;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.Assert;
 import org.testng.AssertJUnit;
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 import argos.saleslogix.selenium.test.CommonNavigation;
 import argos.saleslogix.selenium.test.HeaderButton;
@@ -30,110 +16,6 @@ public class SpeedSearchTest extends BrowserSetup {
 NavButton navbutton = PageFactory.initElements(driver, NavButton.class);
 CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.class);
 
-
-	@Test(enabled = true)
-	public void test00_Mobile_Login() throws InterruptedException {
-		String methodID = "test00_Mobile_Login";
-		
-		SLXMobileLogin slxmobilelogin = PageFactory.initElements(driver, SLXMobileLogin.class);	
-		
-		System.out.println(STARTLINE + " " + methodID + " " + STARTLINE);	
-		//VP: the Mobile Login screen is loaded from base URL
-		for (int second = 0;; second++) {
-			if (second >= 60) AssertJUnit.fail("timeout");
-			try { if (fullProdName.equals(driver.findElement(By.id("pageTitle")).getText()))
-				System.out.println("VP: " + fullProdName + " - Mobile Client load check - Passed");
-				break; 
-			} catch (Exception e) {
-			System.out.println("Error: " + fullProdName + " - Mobile Client load check - FAILED");
-			}
-			Thread.sleep(1000);
-		}
-		
-		//VP: Page Title
-		Thread.sleep(1000);
-		try { assertEquals(shortProdName, driver.getTitle());
-			System.out.println("VP: Login Screen Title check - Passed");
-			} catch (Error e) {
-			System.out.println("Error: Login Screen Title check - FAILED");
-			verificationErrors.append(e.toString());
-		}
-		
-		//VP: Login Page Name
-		Thread.sleep(1000);
-		for (int second = 0;; second++) {
-			if (second >= 60) AssertJUnit.fail("timeout");
-			try { if (fullProdName.equals(driver.findElement(By.xpath("//*[@id='pageTitle']")).getText())) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}		
-		try {
-			assertEquals(fullProdName, driver.findElement(By.xpath("//*[@id='pageTitle']")).getText());
-			System.out.println("VP: Login Page Name check - Passed");
-			} catch (Error e) {
-			System.out.println("Error: Login Page Name check - FAILED");
-			verificationErrors.append(e.toString());
-		}
-		
-		//VP: Copyright Info...
-		try {
-			assertEquals(copyrightLabel, driver.findElement(By.xpath(".//*[@id='login']/span[1]")).getText());
-			System.out.println("VP: Copyright check - Passed");
-			} catch (Error e) {
-			System.out.println("Error: Copyright check - FAILED");
-			verificationErrors.append(e.toString());
-		}
-		try {
-			assertEquals(versionLabel, driver.findElement(By.xpath(".//*[@id='login']/span[2]")).getText());
-			System.out.println("VP: Version Label check - Passed");
-			} catch (Error e) {
-			System.out.println("Error: Version Label check - FAILED");
-			verificationErrors.append(e.toString());
-		}
-		
-		// Step: Enter username and password then click the logon button		
-		slxmobilelogin.doLogin(userName, userPwd, true);
-		
-		// VP: confirm that the 'My Activities' screen displays after login
-		Thread.sleep(3000);
-		try {
-			assertTrue(driver.findElement(By.xpath(".//*[@id='myactivity_list']")).isDisplayed());
-			System.out.println("VP: Successfully logged in to Mobile Client.");
-		} catch (Error e) {
-			closeAlert();
-			System.out.println("Error: Unable to login to Mobile Client.");
-			System.out.println(e.toString());
-		}
-		System.out.println(ENDLINE);	
-	}
-	
-	@Test(enabled = true)
-	public void test99_Mobile_LogOut()  throws InterruptedException {				
-		String methodID = "test99_Mobile_LogOut";
-		
-		CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.class);
-		HeaderButton headerbutton = PageFactory.initElements(driver, HeaderButton.class);
-		
-		System.out.println(STARTLINE + " " + methodID + " " + STARTLINE);
-		// Click the Top-Left, Global Menu button...
-		headerbutton.showGlobalMenu();
-	
-		// Click the Log Off button
-		commNav.clickGlobalMenuItem("log out");
-		Thread.sleep(2000);
-		closeAlert();
-		Thread.sleep(1000);
-					
-		// Verify the Mobile Login screen displays
-		try {
-			AssertJUnit.assertEquals(fullProdName, driver.findElement(By.id("pageTitle")).getText());
-			System.out.println("VP: Mobile Client Logout Check - Passed");
-		} catch (Error e) {     
-			System.out.println("Error: Mobile Client Logout Check - FAILED");
-			System.out.println(e.toString());
-		}
-		System.out.println(ENDLINE);
-	}
-	// *******
 
 	@Test(enabled = true)
 	public void test01_SeTestTCSpeedSearchGeneral() throws Exception {
@@ -375,8 +257,11 @@ CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.cla
 		System.out.println(ENDLINE);
 	}
 	
+	
+	//TODO: complete re-factoring test02_SeTestTCSpeedSearchLeads()
 	@Test(enabled = false)
 	public void test02_SeTestTCSpeedSearchLeads() throws Exception {
+/*		
 	    // SE Test: SETest-TC-SpeedSearch-Leads
 	    // Version: 2.2
 	    // Desc: Search on Leads
@@ -456,10 +341,15 @@ CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.cla
 	    }
 
 	    // -- END
+	     * 
+*/
 	}
 	
+	
+	//TODO: complete re-factoring test03_SeTestTCSpeedSearchAccConActOppNtsHis()	
 	@Test(enabled = false)
 	public void test03_SeTestTCSpeedSearchAccConActOppNtsHis() throws Exception {
+/*		
 	    // SE Test: SETest-TC-SpeedSearch-AccConActOppNtsHis
 	    // Version: 2.2
 	    // Desc: Search on accounts, contacts, activities, opportunities, notes/history
@@ -692,10 +582,15 @@ CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.cla
 	    }
 
 	    // -- END
+	     *
+*/
 	}
+	
 
+	//TODO: complete re-factoring test04_SeTestTCSpeedSearchTickets()
 	@Test(enabled = false)
 	public void test04_SeTestTCSpeedSearchTickets() throws Exception {
+/*		
 	    // SE Test: SETest-TC-SpeedSearch-Leads
 	    // Version: 2.2
 	    // Desc: Search on Tickets
@@ -812,6 +707,113 @@ CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.cla
 		commNav.clickGlobalMenuItem("My Activities");
 		Thread.sleep(3000);
 		
+		System.out.println(ENDLINE);
+*/
+	}
+
+	//Login & Logout
+	//==============
+	@Test(enabled = true)
+	public void test00_Mobile_Login() throws InterruptedException {
+		String methodID = "test00_Mobile_Login";
+		
+		SLXMobileLogin slxmobilelogin = PageFactory.initElements(driver, SLXMobileLogin.class);	
+		
+		System.out.println(STARTLINE + " " + methodID + " " + STARTLINE);	
+		//VP: the Mobile Login screen is loaded from base URL
+		for (int second = 0;; second++) {
+			if (second >= 60) AssertJUnit.fail("timeout");
+			try { if (fullProdName.equals(driver.findElement(By.id("pageTitle")).getText()))
+				System.out.println("VP: " + fullProdName + " - Mobile Client load check - Passed");
+				break; 
+			} catch (Exception e) {
+			System.out.println("Error: " + fullProdName + " - Mobile Client load check - FAILED");
+			}
+			Thread.sleep(1000);
+		}
+		
+		//VP: Page Title
+		Thread.sleep(1000);
+		try { 
+			AssertJUnit.assertEquals(shortProdName, driver.getTitle());
+			System.out.println("VP: Login Screen Title check - Passed");
+			} catch (Error e) {
+			System.out.println("Error: Login Screen Title check - FAILED");
+			verificationErrors.append(e.toString());
+		}
+		
+		//VP: Login Page Name
+		Thread.sleep(1000);
+		for (int second = 0;; second++) {
+			if (second >= 60) AssertJUnit.fail("timeout");
+			try { if (fullProdName.equals(driver.findElement(By.xpath("//*[@id='pageTitle']")).getText())) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}		
+		try {
+			AssertJUnit.assertEquals(fullProdName, driver.findElement(By.xpath("//*[@id='pageTitle']")).getText());
+			System.out.println("VP: Login Page Name check - Passed");
+			} catch (Error e) {
+			System.out.println("Error: Login Page Name check - FAILED");
+			verificationErrors.append(e.toString());
+		}
+		
+		//VP: Copyright Info...
+		try {
+			AssertJUnit.assertEquals(copyrightLabel, driver.findElement(By.xpath(".//*[@id='login']/span[1]")).getText());
+			System.out.println("VP: Copyright check - Passed");
+			} catch (Error e) {
+			System.out.println("Error: Copyright check - FAILED");
+			verificationErrors.append(e.toString());
+		}
+		try {
+			AssertJUnit.assertEquals(versionLabel, driver.findElement(By.xpath(".//*[@id='login']/span[2]")).getText());
+			System.out.println("VP: Version Label check - Passed");
+			} catch (Error e) {
+			System.out.println("Error: Version Label check - FAILED");
+			verificationErrors.append(e.toString());
+		}
+		
+		// Step: Enter username and password then click the logon button		
+		slxmobilelogin.doLogin(userName, userPwd, true);
+		
+		// VP: confirm that the 'My Activities' screen displays after login
+		Thread.sleep(3000);
+		try {
+			AssertJUnit.assertTrue(driver.findElement(By.xpath(".//*[@id='myactivity_list']")).isDisplayed());
+			System.out.println("VP: Successfully logged in to Mobile Client.");
+		} catch (Error e) {
+			closeAlert();
+			System.out.println("Error: Unable to login to Mobile Client.");
+			System.out.println(e.toString());
+		}
+		System.out.println(ENDLINE);	
+	}
+
+	@Test(enabled = true)
+	public void test99_Mobile_LogOut()  throws InterruptedException {				
+		String methodID = "test99_Mobile_LogOut";
+		
+		CommonNavigation commNav = PageFactory.initElements(driver, CommonNavigation.class);
+		HeaderButton headerbutton = PageFactory.initElements(driver, HeaderButton.class);
+		
+		System.out.println(STARTLINE + " " + methodID + " " + STARTLINE);
+		// Click the Top-Left, Global Menu button...
+		headerbutton.showGlobalMenu();
+	
+		// Click the Log Off button
+		commNav.clickGlobalMenuItem("log out");
+		Thread.sleep(2000);
+		closeAlert();
+		Thread.sleep(1000);
+					
+		// Verify the Mobile Login screen displays
+		try {
+			AssertJUnit.assertEquals(fullProdName, driver.findElement(By.id("pageTitle")).getText());
+			System.out.println("VP: Mobile Client Logout Check - Passed");
+		} catch (Error e) {     
+			System.out.println("Error: Mobile Client Logout Check - FAILED");
+			System.out.println(e.toString());
+		}
 		System.out.println(ENDLINE);
 	}
 	
